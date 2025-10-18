ab75cfda6ae06edb20607296977e908017ce5fd1
Nico Sadia <nicosadia17@gmail.com>
2025-10-18 16:24:39 +0100

    chore: stop tracking dist folder

--- dist/lib/constants/spotify.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLAYLIST_URL = exports.RECENTLY_PLAYED_URL = exports.REFRESH_TOKEN_URL = void 0;
-exports.REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token";
-exports.RECENTLY_PLAYED_URL = "https://api.spotify.com/v1/me/player/recently-played";
-exports.PLAYLIST_URL = "https://api.spotify.com/v1/playlists/";
