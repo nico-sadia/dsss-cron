f1355b7fc04ecf7ab708ee8813b1b732b5db060c
Nico Sadia <nicosadia17@gmail.com>
2025-11-23 01:54:07 +0000

    fix: renable cron middleware

--- src/routes/cron/cron.routes.ts
+++ src/routes/cron/cron.routes.ts
@@ -4,14 +4,13 @@ import { addTopPlayed } from "../../controllers/top-played.controller";
 
 const cronRouter = express.Router();
 
-// cronRouter.use((req, res, next) => {
-
-//     const auth = req.headers.authorization;
-//     if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-//         return res.status(403).json({ error: "Forbidden" });
-//     }
-//     next();
-// });
+cronRouter.use((req, res, next) => {
+    const auth = req.headers.authorization;
+    if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(403).json({ error: "Forbidden" });
+    }
+    next();
+});
 
 cronRouter.get("/add-recently-played", addRecentlyPlayed);
 cronRouter.get("/add-top-song", addTopPlayed);
